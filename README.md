e86bd55a0343b637302b8755e43e90d1bae3e00e
Shane Hanna <shane.hanna@gmail.com>
2011-03-23 13:12:04 +1100

    First.

---
+++ README.md
@@ -0,0 +1,19 @@
+jquery-columns
+==============
+
+** Smart-ish column stretching with jQuery **
+
+Synopsis
+--------
+
+    $(function() {
+      var $columns = $('ul#columns');
+      $(window).resize(function() { $columns.columns() }).resize();
+    });
+
+License
+-------
+
+MIT, http://en.wikipedia.org/wiki/MIT_License
+
+Copyright (c) 2011 Stateless Systems, http://statelesssystems.com
---
+++ jquery-columns.js
@@ -0,0 +1,59 @@
+(function($) {
+  var methods = {
+    init: function(options) {
+      return this.each(function() {
+        var $this = $(this), data = $this.data('columns');
+        if (!data) $(this).data('columns', $.extend({target: $this, width: $this.children().first().width()}, options));
+        $this.columns('layout'); // TODO: Pretty sure this isn't the right way to do this.
+      });
+    },
+
+    destroy: function() {
+      return this.each(function() {
+        var $this = $(this), data = $this.data('columns');
+        $(window).unbind('.columns');
+        $this.removeData('columns');
+      })
+    },
+
+    layout: function() {
+      var $this    = $(this),
+          data     = $this.data('columns'),
+          $columns = data.target.children(),
+          $first   = $columns.first();
+
+      $first.css({width: '100%'});
+      data.wrap    = $this.innerWidth(); // WTF: != $first.outerWidth(true)
+      data.columns = Math.floor(data.wrap / data.width);
+
+      data.column             = {};
+      data.column.outer_width = Math.floor(data.wrap / data.columns);
+      data.column.spacing     = $first.outerWidth(true) - $first.width(); // Margin and border.
+      data.column.width       = data.column.outer_width - data.column.spacing;
+      $columns.css({width: data.column.width + 'px'});
+
+      return this;
+    },
+
+    // TODO: Spans are removed on re-layout :(
+    span: function(target, columns) {
+      var $this = $(this), data = $this.data('columns');
+      var width = (columns * data.column.outer_width) - data.column.spacing;
+      target.css({width: width + 'px'});
+      return this;
+    }
+  };
+
+  $.fn.columns = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    }
+    else if (typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
+    }
+    else {
+      $.error('Method ' +  method + ' does not exist on jQuery.columns');
+    }
+  };
+})(jQuery);
+
